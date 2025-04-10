dd3523cabf52ef3aad4376c3c5b3343121a984fb
dane-and <danea@umich.edu>
2025-04-10 07:22:09 -0400

    Create index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+
+declare module 'react-error-boundary' {
+  const value: any;
+  export default value;
+}
+
+declare module 'sanitize-html' {
+  const value: any;
+  export default value;
+}
+
+declare module 'react-quill' {
+  const value: any;
+  export default value;
+}
