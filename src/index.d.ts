8769908cd2e5c1d5d15f2c6cf1a64031e36eb0e7
dane-and <danea@umich.edu>
2025-04-10 08:01:52 -0400

    fixes

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,15 +1,5 @@
+// index.d.ts
 
-declare module 'react-error-boundary' {
-  const value: any;
-  export default value;
-}
-
-declare module 'sanitize-html' {
-  const value: any;
-  export default value;
-}
-
-declare module 'react-quill' {
-  const value: any;
-  export default value;
-}
+declare module 'react-error-boundary';
+declare module 'sanitize-html';
+declare module 'react-quill';
